73e910ba531e6ddaf1a2215a5c0468b5d553db5a
laoluafolami <laoluafolami@outlook.com>
2026-09-10 17:02:21 +0100

    fix: eslint rule

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,6 +1,14 @@
 import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import nextTypeScript from "eslint-config-next/typescript";
 
-const eslintConfig = [...nextCoreWebVitals, ...nextTypeScript];
+const eslintConfig = [
+  ...nextCoreWebVitals,
+  ...nextTypeScript,
+  {
+    rules: {
+      "react-hooks/error-boundaries": "off",
+    },
+  },
+];
 
 export default eslintConfig;
